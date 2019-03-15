013cc5b15465e110253074faa2924ec20bc931b9
Kiliano Lopes <kilianolopes@gmail.com>
2019-03-14 23:34:33 -0300

    Exemplo de json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,48 @@
+var campeonatos = window.localStorage.getItem('campeonatos');
+
+if (campeonatos == null) {
+	campeonatos = {};
+}
+
+// teste
+
+campeonatos = {
+	"mk64" : [
+		{
+			"id" : 123,
+			"nome" : "Campeonato Teste",
+			"status" : "ativo",
+			"participantes" : ["Kiliano", "Murilo", "Roger"],
+			"rodada": [
+				{
+					"1" : "Roger",
+					"2" : "Kiliano",
+					"3" : "Murilo",
+					"4" : ""
+				},
+				{
+					"1" : "Kiliano",
+					"2" : "Roger",
+					"3" : "Murilo",
+					"4" : ""
+				},
+				{
+					"1" : "Roger",
+					"2" : "Murilo",
+					"3" : "Kiliano",
+					"4" : ""
+				}
+			]
+		}
+	]
+};
+
+// fim teste
+
+console.log(campeonatos);
+// window.localStorage.setItem('name', 'Obaseki Nosa');
+// window.localStorage.getItem('campeonatos');
+
 jQuery(document).ready(function(){
+
 })
